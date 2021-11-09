b2bef78a482f2f7268319d72092a61a52f0990e3
averak <abe12@mccc.jp>
2021-11-09 22:02:32 +0900

    [fix] dashborad document in production config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
   production: true,
+
+  // ドキュメント一覧
+  documents: [
+    {
+      title: 'Developer Portal',
+      description: 'Developer Portalは、立命館学生がWeb技術を学ぶためのポータルサイトです。',
+      url: '/dashboard',
+      version: 'v1.0',
+      display: false,
+      externalLink: {
+        reference: '',
+        faq: '',
+      },
+      navs: [],
+    },
+  ],
 };
