b4e069c11bc5338ad872bedeacbc9fc35447b40b
averak <abe12@mccc.jp>
2021-11-10 11:45:23 +0900

    [feat] add new document link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -34,5 +34,60 @@ export const environment = {
         },
       ],
     },
+    {
+      title: 'Webの基本',
+      description: '初心者向けに、Webとは何なのか、何を解決できるのか紹介します。',
+      url: '/web-intro',
+      version: 'v1.0',
+      display: true,
+      externalLink: {
+        reference: '',
+        faq: '',
+      },
+      navs: [
+        {
+          label: 'Webの基本',
+          url: '/web-intro',
+          icon: 'article',
+        },
+      ],
+    },
+    {
+      title: 'モダンなWeb技術',
+      description:
+        'Webは進化が著しい技術分野です。そこで、ここ数年の最新トレンドについて紹介していきます。',
+      url: '/modern-web',
+      version: 'v1.0',
+      display: true,
+      externalLink: {
+        reference: '',
+        faq: '',
+      },
+      navs: [
+        {
+          label: 'モダンなWeb技術',
+          url: '/modern-web',
+          icon: 'article',
+        },
+      ],
+    },
+    {
+      title: '本ページを支える技術',
+      description: 'Developer Portalを支える技術を紹介します。',
+      url: '/about',
+      version: 'v1.0',
+      display: true,
+      externalLink: {
+        reference: '',
+        faq: '',
+      },
+      navs: [
+        {
+          label: '技術構成',
+          url: '/about',
+          icon: 'article',
+        },
+      ],
+    },
   ],
 };
